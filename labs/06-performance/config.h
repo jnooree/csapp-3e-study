1eaba6c583284f8d7a8fbeaab8fb89284dd14c5a
jnooree <jnooree@snu.ac.kr>
2023-01-03 19:43:50 +0900

    style(06-perf): re-format files

--- labs/06-performance/config.h
+++ labs/06-performance/config.h
@@ -4,11 +4,11 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-/* 
+/*
  * CPEs for the baseline (naive) version of the rotate function that
  * was handed out to the students. Rd is the measured CPE for a dxd
  * image. Run the driver.c program on your system to get these
- * numbers.  
+ * numbers.
  */
 #define R64    14.7
 #define R128   40.1
@@ -16,11 +16,11 @@
 #define R512   65.9
 #define R1024  94.5
 
-/* 
+/*
  * CPEs for the baseline (naive) version of the smooth function that
  * was handed out to the students. Sd is the measure CPE for a dxd
  * image. Run the driver.c program on your system to get these
- * numbers.  
+ * numbers.
  */
 #define S32   695
 #define S64   698
