5463cfa1a8eeaa27f086903fd970ae9e21533e08
jnooree <jnooree@snu.ac.kr>
2023-01-03 13:34:37 +0900

    fix(05-cache): add missing extern directives

--- labs/05-cache/cachelab.h
+++ labs/05-cache/cachelab.h
@@ -20,18 +20,19 @@ typedef struct trans_func {
  * printSummary - This function provides a standard way for your cache
  * simulator * to display its final hit and miss statistics
  */
-void printSummary(int hits,       /* number of  hits */
-                  int misses,     /* number of misses */
-                  int evictions); /* number of evictions */
+extern void printSummary(int hits,       /* number of  hits */
+                         int misses,     /* number of misses */
+                         int evictions); /* number of evictions */
 
 /* Fill the matrix with data */
-void initMatrix(int M, int N, int A[N][M], int B[M][N]);
+extern void initMatrix(int M, int N, int A[N][M], int B[M][N]);
 
 /* The baseline trans function that produces correct results. */
-void correctTrans(int M, int N, int A[N][M], int B[M][N]);
+extern void correctTrans(int M, int N, int A[N][M], int B[M][N]);
 
 /* Add the given function to the function list */
-void registerTransFunction(void (*trans)(int M, int N, int[N][M], int[M][N]),
-                           char *desc);
+extern void registerTransFunction(void (*trans)(int M, int N, int[N][M],
+                                                int[M][N]),
+                                  char *desc);
 
 #endif /* CACHELAB_TOOLS_H */
